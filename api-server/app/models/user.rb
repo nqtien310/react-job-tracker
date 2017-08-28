7171353e134e79b4c4470933215b598d5da05cf0
Tien Nguyen <nqtien310@gmail.com>
2017-08-28 15:16:12 +0700

    User has role (ADMIN|USER|MANAGER)

--- api-server/app/models/user.rb
+++ api-server/app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  ROLES = [
+    ROLE_USER    = 'user',
+    ROLE_MANAGER = 'manager',
+    ROLE_ADMIN   = 'admin'
+  ]
+
   validates :email, presence: true, uniqueness: true
   validates :full_name, presence: true
+  validates :role, presence: true, inclusion: {in:  ROLES}
   has_secure_password
 end
--- api-server/spec/models/user_spec.rb
+++ api-server/spec/models/user_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:full_name) }
+  it { should validate_confirmation_of(:password) }
   it { should have_secure_password }
+  it { should validate_presence_of(:role) }
+  it { should validate_inclusion_of(:role).in_array(['user', 'manager', 'admin']) }
 end
