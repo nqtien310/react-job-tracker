9f60cb0ca430823a50ddbf59b6986388cbfb0cb7
Tien Nguyen <nqtien310@gmail.com>
2017-09-01 18:28:52 +0700

    return entry's date

--- api-server/app/serializers/entry_serializer.rb
+++ api-server/app/serializers/entry_serializer.rb
@@ -1,3 +1,7 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :time_in_second, :distance_in_metre, :formatted_date, :speed
+  attributes :id, :time_in_second, :distance_in_metre, :formatted_date, :speed, :date
+
+  def date
+    object.formatted_date
+  end
 end
