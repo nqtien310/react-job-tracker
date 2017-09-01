58370150c34d110c385f8df95021404b7af1476a
Tien Nguyen <nqtien310@gmail.com>
2017-09-01 09:14:06 +0700

    handle missing root param

--- api-server/app/controllers/application_controller.rb
+++ api-server/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include Concerns::Authenticatable
+  rescue_from ActionController::ParameterMissing, with: :param_missing_handler
 
   before_action :authenticate!
 
@@ -18,5 +19,11 @@ class ApplicationController < ActionController::API
     if current_user != @user && !current_user.role.in?(roles)
       error_render('UNAUTHORIZE')
     end
+  end
+
+  private
+
+  def param_missing_handler(e)
+    error_render(e.message)
   end
 end
